1d56c4230e38abef6382d200b28de3000a921295
arisztid munoz <arisztidmunoz@arisztids-MacBook-Pro.local>
2018-08-21 09:24:33 -0700

    wpx6 done

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ import About from './About/About';
 import {Route, Link} from 'react-router-dom'
 import FAQ from './About/FAQ/FAQ';
 import Company from './About/Company/Company';
+import People from './People/People';
+import Character from './Character/Character'
+
 
 class App extends Component {
   render() {
@@ -13,12 +16,15 @@ class App extends Component {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
+          <Link to="/people">people</Link>
         </nav> 
         <Route exact path='/' component={Home}/>
         <Route path='/about' render={()=> <About>
         <Route path='/about/faq' component={FAQ}/>
         <Route path='/about/company' component={Company}/>
         </About>}/>
+        <Route exact path='/people' component={People}/>
+        <Route path='/people/:id' component={Character}/>
       </div>
     );
   }
