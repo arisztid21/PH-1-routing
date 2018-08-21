9b2cc3069fc3c61d54ea68dd986bd8ec4c2cee12
arisztid munoz <arisztidmunoz@arisztids-MacBook-Pro.local>
2018-08-21 11:52:25 -0700

    add sub routes wpx7

--- __instructor-code__/src/components/App.js
+++ __instructor-code__/src/components/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import Home from './Home/Home';
 import About from './About/About';
+import FAQ from './About/FAQ/FAQ';
+import Company from './About/Company/Company';
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
-  componentDidMount() {
-    window.addEventListener('hashchange', () => this.forceUpdate());
-  }
-  
   render() {
     return (
       <div>
         This is App.js
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
         <div>
-          <a href="/#">Home</a>
-          <a href="/#/about">About</a>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" render={() => <About>
+            <Route path="/about/faq" component={FAQ} />
+            <Route path="/about/company" component={Company} />
+          </About>} />
         </div>
-        {window.location.hash === '#/about' ? <About /> : <Home />}
       </div>
     );
   }
--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import Home from './Home/Home';
 import About from './About/About';
+import {Route, Link} from 'react-router-dom';
+import FAQ from './About/FAQ/FAQ';
+import Company from './About/Company/Company';
 
 class App extends Component {
-  componentDidMount(){
-    window.addEventListener('hashchange', () => this.forceUpdate())
-  }
   render() {
     return (
       <div>
         This is App.js
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+        </nav>
         <div>
-          <a href='/#'>Home</a>
-          <a href='/#/about'>About</a>
+          <Route path='/' exact component={Home}/>
+          <Route path='/about' render={() => <About>
+            <Route path='/about/faq' component={FAQ}/>
+            <Route path='/about/company' component={Company}/>
+          </About>}/>
         </div>
-        {window.location.hash === '#/about' ? <About/> : <Home/>}
       </div>
     );
   }
