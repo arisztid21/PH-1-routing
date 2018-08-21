ceafb12e9888190e54854ddd9cdfb5b245a403c0
arisztid munoz <arisztidmunoz@arisztids-MacBook-Pro.local>
2018-08-21 10:44:32 -0700

    initial commit wpx7

--- __instructor-code__/src/components/App.js
+++ __instructor-code__/src/components/App.js
@@ -3,18 +3,19 @@ import Home from './Home/Home';
 import About from './About/About';
 
 class App extends Component {
-  componentDidMount(){
-    window.addEventListener('hashchange', () => this.forceUpdate())
+  componentDidMount() {
+    window.addEventListener('hashchange', () => this.forceUpdate());
   }
+  
   render() {
     return (
       <div>
         This is App.js
         <div>
-          <a href='/#'>Home</a>
-          <a href='/#/about'>About</a>
+          <a href="/#">Home</a>
+          <a href="/#/about">About</a>
         </div>
-        {window.location.hash === '#/about' ? <About/> : <Home/>}
+        {window.location.hash === '#/about' ? <About /> : <Home />}
       </div>
     );
   }
